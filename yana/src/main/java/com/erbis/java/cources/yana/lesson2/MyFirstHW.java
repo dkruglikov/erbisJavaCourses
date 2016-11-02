2240b5b904d308ce26bc0eb10c1b42398965abca
yana <yana@yanas-mbp>
2016-11-02 19:58:08 +0200

    My third commit

--- yana/src/main/java/com/erbis/java/cources/yana/lesson2/MyFirstHW.java
+++ yana/src/main/java/com/erbis/java/cources/yana/lesson2/MyFirstHW.java
@@ -1,5 +1,10 @@
 package com.erbis.java.cources.yana.lesson2;
 
 public class MyFirstHW {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		for (int i = 0; i < args.length; i++)
+			System.out.println(args[i]);
+	}
 
 }
