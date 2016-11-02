d4a3f63abf7186453c6b96c525adbefd73363d4e
Max <maksym.podturkin@erbiscloud.com>
2016-11-02 17:05:52 +0200

    My first home work

---
+++ max/src/main/java/com/erbis/java/courses/max/MySecondApp.java
@@ -0,0 +1,12 @@
+
+package com.erbis.java.courses.max;
+
+public class MySecondApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
