81b18519b6ce06b5e34485e2b49f7080ca7a3904
Admin <Admin@Lenovo-PC>
2016-11-02 19:08:05 +0200

    kjj

---
+++ ira/src/main/java/com/erbis/java/courses/ira/MySecondApp.java
@@ -0,0 +1,11 @@
+package com.erbis.java.courses.ira;
+
+
+public class MySecondApp {
+	public static void main(String[] args) {
+		System.out.println("Hello,world!");
+		for (int i=0; i<args.length; i++){
+		System.out.println(args[i]);	
+		}
+	}
+}
