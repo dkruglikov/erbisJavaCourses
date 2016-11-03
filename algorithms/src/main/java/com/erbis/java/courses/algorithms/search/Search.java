2a84d6dbc6437f912cc0bced7f4dc04f0bc36006
Dmitry Kruglikov <dmitry.kruglikov@erbis.cloud>
2016-11-03 13:45:52 +0200

    Search stub added

---
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/search/Search.java
@@ -0,0 +1,112 @@
+package com.erbis.java.courses.algorithms.search;
+
+public class Search {
+	
+	public static int search(byte[] array, byte value) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int search(short[] array, short value) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int search(int[] array, int value) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int search(long[] array, long value) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int search(char[] array, char value) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int search(float[] array, double value) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static byte min(byte[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static short min(short[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int min(int[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static long min(long[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static char min(char[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static float min(float[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double min(double[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static byte max(byte[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static short max(short[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static int max(int[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static long max(long[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static char max(char[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static float max(float[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double max(double[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double avg(byte[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double avg(short[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double avg(int[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double avg(long[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double avg(char[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double avg(float[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static double avg(double[] array) {
+		throw new UnsupportedOperationException();
+	}
+}
