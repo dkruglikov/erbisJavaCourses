0caf4daa3be544bbf101de8dfa71397705a4c2cf
yana <yana@yanas-mbp>
2016-11-17 15:18:27 +0200

    lesson4

--- yana/src/main/java/MyFirstApp.java
+++ yana/src/main/java/MyFirstApp.java
@@ -1,8 +1,26 @@
 
 public class MyFirstApp {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		for (int i=0;i<args.length; i++)
-			System.out.println(args[i]);
+		byte b = 1;
+		System.out.println(b);
+		short s = 255;
+		System.out.println(s);
+		int i = -17;
+		System.out.println(i);
+		long l = 177;
+		System.out.println(l);
+		char c = 'Y';
+		System.out.println(c);
+		float f = 5.5F;
+		System.out.println(f);
+		double d = 8.7;
+		System.out.println(d);
+		int V0 = 0;
+		float V1 = 1;
+		V1 = V0;
+		//V0 = V1;
+		int i1 = 0x7B;
+		System.out.println(i1);
 	}
+
 }
