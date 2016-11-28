384a07b6c7e9a4679323522c752c7bc4d29a993a
erbis <erbis@erbis-QA1>
2016-11-28 13:46:10 +0200

    The third HW

---
+++ olga/src/main/java/lesson4/MyThirdHW.java
@@ -0,0 +1,21 @@
+package lesson4;
+
+public class MyThirdHW {
+	public static void main(String[] args) {
+
+		int i = 55;
+		char c = 'е';
+		long l = 456666;
+		float f = 10.1F;
+		double d = 588787.65464;
+		boolean a = (10 * 2) == 20;
+		boolean b = (45 * 2) == 50;
+		boolean g = (i * l) == 50;
+
+		System.out.println(c<f);
+		System.out.println(i>l);
+		System.out.println(a||b);
+		System.out.println(g^a);
+		System.out.println(b&&g);
+	}
+}
