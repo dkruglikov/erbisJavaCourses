f4f5650477fe49ceadc68c677db3cfa98ad1273c
yana <yana@yanas-mbp>
2016-12-08 13:52:55 +0200

    Search, min, max

--- algorithms/src/main/java/com/erbis/java/courses/algorithms/search/Search.java
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/search/Search.java
@@ -1,121 +1,242 @@
 package com.erbis.java.courses.algorithms.search;
 
+import java.util.NoSuchElementException;
+
 public class Search {
-	
 	public static final int RESULT_ELEMENT_NOT_FOUND = -1;
-	
+
 	private Search() {
 	}
-	
+
 	public static int search(byte[] array, byte value) {
-		throw new UnsupportedOperationException();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
-	
+
 	public static int search(short[] array, short value) {
-		throw new UnsupportedOperationException();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
-	
+
 	public static int search(int[] array, int value) {
-		throw new UnsupportedOperationException();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
-	
+
 	public static int search(long[] array, long value) {
-		throw new UnsupportedOperationException();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
-	
+
 	public static int search(char[] array, char value) {
-		throw new UnsupportedOperationException();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
-	
+
 	public static int search(float[] array, float value) {
-		throw new UnsupportedOperationException();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
-	
+
 	public static int search(double[] array, double value) {
-		throw new UnsupportedOperationException();
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}
+		throw new NoSuchElementException();
 	}
-	
+
 	public static byte min(byte[] array) {
-		throw new UnsupportedOperationException();
+		int iMin = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[iMin]) {
+				iMin = i;
+			}
+		}
+		return array[iMin];
+		
 	}
-	
+
 	public static short min(short[] array) {
-		throw new UnsupportedOperationException();
+		int iMin = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[iMin]) {
+				iMin = i;
+			}
+		}
+		return array[iMin];
 	}
-	
+
 	public static int min(int[] array) {
-		throw new UnsupportedOperationException();
+		int iMin = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[iMin]) {
+				iMin = i;
+			}
+		}
+		return array[iMin];
 	}
-	
+
 	public static long min(long[] array) {
-		throw new UnsupportedOperationException();
+		int iMin = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[iMin]) {
+				iMin = i;
+			}
+		}
+		return array[iMin];
 	}
-	
+
 	public static char min(char[] array) {
-		throw new UnsupportedOperationException();
+		int iMin = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[iMin]) {
+				iMin = i;
+			}
+		}
+		return array[iMin];
 	}
-	
+
 	public static float min(float[] array) {
-		throw new UnsupportedOperationException();
+		int iMin = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[iMin]) {
+				iMin = i;
+			}
+		}
+		return array[iMin];
 	}
-	
+
 	public static double min(double[] array) {
-		throw new UnsupportedOperationException();
+		int iMin = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[iMin]) {
+				iMin = i;
+			}
+		}
+		return array[iMin];
 	}
-	
+
 	public static byte max(byte[] array) {
-		throw new UnsupportedOperationException();
+		int iMax = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[iMax]) {
+				iMax = i;
+			}
+		}
+		return array[iMax];
 	}
-	
+
 	public static short max(short[] array) {
-		throw new UnsupportedOperationException();
+		int iMax = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[iMax]) {
+				iMax = i;
+			}
+		}
+		return array[iMax];
 	}
-	
+
 	public static int max(int[] array) {
-		throw new UnsupportedOperationException();
+		int iMax = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[iMax]) {
+				iMax = i;
+			}
+		}
+		return array[iMax];
 	}
-	
+
 	public static long max(long[] array) {
-		throw new UnsupportedOperationException();
+		int iMax = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[iMax]) {
+				iMax = i;
+			}
+		}
+		return array[iMax];
 	}
-	
+
 	public static char max(char[] array) {
-		throw new UnsupportedOperationException();
+		int iMax = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[iMax]) {
+				iMax = i;
+			}
+		}
+		return array[iMax];
 	}
-	
+
 	public static float max(float[] array) {
-		throw new UnsupportedOperationException();
+		int iMax = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[iMax]) {
+				iMax = i;
+			}
+		}
+		return array[iMax];
 	}
-	
+
 	public static double max(double[] array) {
-		throw new UnsupportedOperationException();
+		int iMax = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[iMax]) {
+				iMax = i;
+			}
+		}
+		return array[iMax];
 	}
-	
-	public static double avg(byte[] array) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static double avg(short[] array) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static double avg(int[] array) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static double avg(long[] array) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static double avg(char[] array) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static double avg(float[] array) {
-		throw new UnsupportedOperationException();
-	}
-	
-	public static double avg(double[] array) {
-		throw new UnsupportedOperationException();
-	}
+
+//	public static double avg(byte[] array) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static double avg(short[] array) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static double avg(int[] array) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static double avg(long[] array) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static double avg(char[] array) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static double avg(float[] array) {
+//		throw new UnsupportedOperationException();
+//	}
+//
+//	public static double avg(double[] array) {
+//		throw new UnsupportedOperationException();
+//	}
 }
