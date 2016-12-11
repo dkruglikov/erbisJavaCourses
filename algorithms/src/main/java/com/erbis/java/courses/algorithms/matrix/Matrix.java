1061935bf803d0da61b031e2ade8492b72a53cd3
erbis <erbis@erbis-QA1>
2016-12-12 00:11:21 +0200

    HW6

--- algorithms/src/main/java/com/erbis/java/courses/algorithms/matrix/Matrix.java
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/matrix/Matrix.java
@@ -1,47 +1,138 @@
 package com.erbis.java.courses.algorithms.matrix;
 
 public final class Matrix {
-	
+
 	private Matrix() {
 	}
-	
+
 	public static double[][] create(double[] array, int width, int height) {
-		return null;
+		double[][] matrix1 = new double[height][width];
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+
+				int k = i * width + j;
+				if (k < array.length) {
+					matrix1[i][j] = array[k];
+				}
+			}
+		}
+		return matrix1;
 	}
-	
+
 	public static double sum(double[][] matrix) {
-		return 0;
+		double sum = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				sum += matrix[i][j];
+			}
+		}
+		return sum;
 	}
-	
+
+	// as I understood it should be sum of abs
 	public static double sumAbs(double[][] matrix) {
-		return 0;
+		double sumA = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				double a = matrix[i][j];
+				if (a > 0) {
+					sumA += a;
+				} else {
+					sumA -= a;
+				}
+			}
+		}
+		return sumA;
+
 	}
-	
+
+	// it works only for rectangular matrixes
 	public static double mainDiagonalSum(double[][] matrix) {
-		return 0;
+		double sumD = 0;
+		for (int i = 0; i < matrix.length && i < matrix[i].length; i++) {
+			sumD += matrix[i][i];
+		}
+		return sumD;
 	}
-	
+
 	public static double mainDiagonalAndAboveSum(double[][] matrix) {
-		return 0;
+		double sumDA = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (i <= j) {
+					sumDA += matrix[i][j];
+				}
+			}
+		}
+		return sumDA;
 	}
-	
+
 	public static double sideDiagonalSum(double[][] matrix) {
-		return 0;
+		double sumSD = 0;
+		for (int i = 0; i < matrix.length && i < matrix[i].length; i++) {
+			sumSD += matrix[i][matrix[i].length - i];
+		}
+		return sumSD;
 	}
-	
+
 	public static double max(double[][] matrix) {
-		return 0;
+		double max = Double.MIN_VALUE;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (max < matrix[i][j]) {
+					max = matrix[i][j];
+				}
+			}
+		}
+		return max;
 	}
-	
+
 	public static double min(double[][] matrix) {
-		return 0;
+		double min = Double.MAX_VALUE;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (min > matrix[i][j]) {
+					min = matrix[i][j];
+				}
+			}
+		}
+		return min;
 	}
-	
+
 	public static double[] maxRow(double[][] matrix) {
-		return null;
+		double maxSumR = Double.MIN_VALUE;
+		int maxI = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			double sumR = 0;
+			for (int j = 0; j < matrix[i].length; j++) {
+				sumR += matrix[i][j];
+			}
+			if (maxSumR < sumR) {
+				maxSumR = sumR;
+				maxI = i;
+			}
+		}
+		return matrix[maxI];
 	}
-	
+
+	// it works only for rectangular matrixes
 	public static double[] maxColumn(double[][] matrix) {
-		return null;
+		double maxSumC = Double.MIN_VALUE;
+		int maxJ = 0;
+		for (int j = 0; j < matrix[0].length; j++) {
+			double sumC = 0;
+			for (int i = 0; i < matrix.length; i++) {
+				sumC += matrix[i][j];
+			}
+			if (maxSumC < sumC) {
+				maxSumC = sumC;
+				maxJ = j;
+			}
+		}
+		double[] arrMC = new double[matrix.length];
+		for (int i = 0; i < matrix.length; i++) {
+			arrMC[i] = matrix[i][maxJ];
+		}
+		return arrMC;
 	}
 }
--- algorithms/src/main/java/com/erbis/java/courses/algorithms/strings/StringUtil.java
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/strings/StringUtil.java
@@ -1,23 +1,49 @@
 package com.erbis.java.courses.algorithms.strings;
 
 public final class StringUtil {
-	
+
 	private StringUtil() {
 	}
-	
+
 	public static char getMostRepeatableChar(String s) {
-		return 0;
+		int maxCounter = 1;
+		char mostRepeatable = s.charAt(0);
+		for (int i = 0; i < s.length(); i++) {
+			int counter = 1;
+			for (int j = i + 1; j < s.length(); j++) {
+				if (s.charAt(i) == s.charAt(j)) {
+					counter++;
+				}
+			}
+			if (maxCounter < counter) {
+				maxCounter = counter;
+				mostRepeatable = s.charAt(i);
+			}
+		}
+		return mostRepeatable;
 	}
-	
+
 	public static int getCharCount(String s) {
-		return 0;
+		return s.length();
 	}
-	
+
 	public static String getLongestWord(String s) {
-		return null;
+		String longest = "";
+		String word = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ' || i == s.length() - 1) {
+				if (word.length() > longest.length()) {
+					longest = word;
+				}
+				word = "";
+			} else {
+				word += s.charAt(i);
+			}
+		}
+		return longest;
 	}
-	
+
 	public static char getLastChar(String s) {
-		return 0;
+		return s.charAt(s.length() - 1);
 	}
 }
