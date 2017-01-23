4573bd4cc8f0de274c69235eabd2e050db465122
Dmitry Kruglikov <dmitry.kruglikov@erbis.cloud>
2017-01-23 13:39:30 +0200

    Sorter added

---
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/sort/SortStat.java
@@ -0,0 +1,41 @@
+package com.erbis.java.courses.algorithms.sort;
+
+public class SortStat {
+	
+	private int size;
+	private int comparisons;
+	private int swaps;
+	private long time;
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getComparisons() {
+		return comparisons;
+	}
+
+	public void setComparisons(int comparisons) {
+		this.comparisons = comparisons;
+	}
+
+	public int getSwaps() {
+		return swaps;
+	}
+
+	public void setSwaps(int swaps) {
+		this.swaps = swaps;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+}
