7d5f9951406fc3a1cd5b2df6b2fa6e7203e7b26b
Dmitry Kruglikov <dmitry.kruglikov@erbis.cloud>
2017-02-07 12:42:32 +0200

    InsertionSorter added

--- algorithms/src/main/java/com/erbis/java/courses/algorithms/sort/InsertionSorter.java
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/sort/InsertionSorter.java
@@ -1,20 +1,17 @@
 package com.erbis.java.courses.algorithms.sort;
 
-public class InsertionSorter {
-	
-	public SortStat sort(int[] arrsy) {
-		throw new UnsupportedOperationException();
-	}
-	
-	protected int compare(int[] array, int i0, int i1, SortStat sortStat) {
-		sortStat.setComparisons(sortStat.getComparisons() + 1);
-		return Integer.compare(array[i0], array[i1]);
-	}
-	
-	protected void swap(int[] array, int i0, int i1, SortStat sortStat) {
-		sortStat.setSwaps(sortStat.getSwaps() + 1);
-		int temp = array[i0];
-		array[i0] = array[i1];
-		array[i1] = temp;
+public class InsertionSorter extends Sorter {
+
+	@Override
+	protected void sort(int[] array, SortStat sortStat) {
+		for (int i = 1; i < array.length; i++) {
+			int j = i;
+			while (j > 0 && compare(array, j - 1, i, sortStat) > 0) {
+				j--;
+			}
+			if (i != j) {
+				move(array, i, j, sortStat);
+			}
+		}
 	}
 }
