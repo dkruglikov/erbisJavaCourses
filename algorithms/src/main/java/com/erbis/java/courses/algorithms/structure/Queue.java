4b15ea60b04ca7f5d52ce96b7f1270ef3ba15761
Dmitry Kruglikov <dmitry.kruglikov@erbis.cloud>
2017-03-06 13:35:47 +0200

    Queue interface added

---
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/structure/Queue.java
@@ -0,0 +1,33 @@
+package com.erbis.java.courses.algorithms.structure;
+
+/**
+ * LIFO queue conract.
+ * @author dkruglikov
+ */
+public interface Queue {
+	
+	/**
+	 * Returns queue size.
+	 * @return size
+	 */
+	int size();
+	
+	/**
+	 * Checks if queue empty.
+	 * @return <code>true</code> if queue empty
+	 * and <code>false</code> otherwise.
+	 */
+	boolean isEmpty();
+	
+	/**
+	 * Adds element to the end of queue.
+	 * @param element element to add
+	 */
+	void add(String element);
+	
+	/**
+	 * Returns head of queue and removes element from it.
+	 * @return head of queue
+	 */
+	String poll();
+}
