76eedf4baf777240a64ebf515dfea7f8220a9469
yana <yana@yanas-mbp>
2017-03-15 18:30:36 +0200

    Implementation of poll method in QueueImpl class. Fix checkstyle errors.

--- algorithms/src/main/java/com/erbis/java/courses/algorithms/structure/impl/QueueImpl.java
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/structure/impl/QueueImpl.java
@@ -2,52 +2,54 @@ package com.erbis.java.courses.algorithms.structure.impl;
 
 import com.erbis.java.courses.algorithms.structure.Queue;
 
-public class QueueImpl 
-implements Queue {
-	
-	private QueueElement head;
-	private int size;
+public class QueueImpl implements Queue {
 
-	@Override
-	public int size() {
-		int size = 0;
-		QueueElement carret = head;
-		while(carret != null){
-			size++;
-			carret = carret.getNext();
-		}
-		return size;
-	}
+    private QueueElement head;
+    private int size;
 
-	@Override
-	public boolean isEmpty() {
-		return head == null;
-	}
+    @Override
+    public int size() {
+        QueueElement carret = head;
+        while (carret != null) {
+            size++;
+            carret = carret.getNext();
+        }
+        return size;
+    }
 
-	@Override
-	public void add(String element) {
-		QueueElement tail = new QueueElement(element);
-		if (head == null) {
-			head = tail;
-		}
-		else {
-			QueueElement last = getLast();
-			last.setNext(tail);
-		}
-	}
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
 
-	@Override
-	public String poll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	private QueueElement getLast() {
-		QueueElement carret = head;
-		while (carret.getNext() != null) {
-			carret = carret.getNext();
-		}
-		return carret;
-	}
+    @Override
+    public void add(String element) {
+        QueueElement tail = new QueueElement(element);
+        if (head == null) {
+            head = tail;
+        } else {
+            QueueElement last = getLast();
+            last.setNext(tail);
+        }
+    }
+
+    @Override
+    public String poll() {
+        if (head != null) {
+            String result = head.getValue();
+            head = head.getNext();
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    private QueueElement getLast() {
+        QueueElement carret = head;
+        while (carret.getNext() != null) {
+            carret = carret.getNext();
+        }
+        return carret;
+    }
 
 }
