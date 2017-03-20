af1dce9c59812c9a9a503a8616ef278ed88bca44
Dmitry Kruglikov <dmitry.kruglikov@erbis.cloud>
2017-03-20 15:57:02 +0200

    Javadoc added for documents module

--- document/src/main/java/com/erbis/java/courses/document/RandomDocumentFactory.java
+++ document/src/main/java/com/erbis/java/courses/document/RandomDocumentFactory.java
@@ -2,6 +2,11 @@ package com.erbis.java.courses.document;
 
 import java.util.Random;
 
+/**
+ * Document factory that creates random <code>{@link Document}</code> using predefined set of parameters
+ * (like title, author etc.)
+ * @author dkruglikov
+ */
 public class RandomDocumentFactory {
 	
 	private static final String[] TITLES = {"Algebra", "Algorithms", "Arithmetics", "Astrology", "Astronomy",
@@ -11,6 +16,10 @@ public class RandomDocumentFactory {
 	private static final String[] TYPES = {"Mathematics", "Others", "Philosophy", "Sport"};
 	private final Random random = new Random();
 	
+	/**
+	 * Creates new random document.
+	 * @return document
+	 */
 	public Document create() {
 		String title = TITLES[random.nextInt(TITLES.length)];
 		String author = AUTHORS[random.nextInt(AUTHORS.length)];
