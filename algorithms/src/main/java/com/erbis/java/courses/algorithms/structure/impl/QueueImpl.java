17c8a1c5273338fb1fea84138b315b5152814896
Dmitry Kruglikov <dmitry.kruglikov@erbis.cloud>
2017-08-17 14:38:41 +0300

    Linked list test updated

--- algorithms/src/main/java/com/erbis/java/courses/algorithms/structure/impl/QueueImpl.java
+++ algorithms/src/main/java/com/erbis/java/courses/algorithms/structure/impl/QueueImpl.java
@@ -79,4 +79,14 @@ public class QueueImpl<E> implements Queue<E> {
             }
         } 
     }
+	
+	protected void insertHead(E element) {
+		if (head == null) {
+			add(element);
+		} else {
+			QueueElement<E> oldHead = head;
+			head = new QueueElement<>(element);
+			head.setNext(oldHead);
+		}
+	}
 }
